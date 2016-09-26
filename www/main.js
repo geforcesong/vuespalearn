94bc20cad8c5023d006efb8cf0da5cbe1b8db6a5
gguo <gguo@movoto.com>
2016-09-26 09:49:59 +0800

    tried routes

--- www/main.js
+++ www/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-class App{
-    constructor(){
-         Vue.use(VueRouter);
+class App {
+    constructor() {
+        Vue.use(VueRouter);
     }
 
-    run(){
-        console.log("hello vue");
+    run() {
+        var Foo = {
+            template: '<p>This is foo!</p>'
+        }
+
+        var Bar = {
+            template: '<p>This is bar!</p>'
+        }
+
+        var App = {};
+        var router = new VueRouter();
+        router.map({
+            '/foo': {
+                component: Foo
+            },
+            '/bar': {
+                component: Bar
+            }
+        });
+        router.start(App, '#app')
     }
 }
 
